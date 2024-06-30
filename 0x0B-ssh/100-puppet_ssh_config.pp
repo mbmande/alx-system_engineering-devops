82ad6ebe5df5907f13b01656d8e0cccb36d1fc6f
mbmande <mbsmande@gmail.com>
2024-06-30 11:32:49 -0700

    my puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
+# hfeuhgjelfke
 file { '/etc/ssh/ssh_config':
   ensure  => present,
-  content => template('module_name/ssh_config.erb'),
+  content => "\
+# Managed by Puppet
+PasswordAuthentication no
+IdentityFile ~/.ssh/school
+",
 }
-
-# Assuming 'module_name/templates/ssh_config.erb' contains:
-#   # Managed by Puppet
-#   PasswordAuthentication no
-#   IdentityFile ~/.ssh/school
