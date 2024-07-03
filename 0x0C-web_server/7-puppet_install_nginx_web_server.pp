e32604569ae347ca1478ffe187cbfd31eed008fb
mbmande <mbsmande@gmail.com>
2024-07-02 17:57:28 -0700

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,51 +1,25 @@
 #!/usr/bin/env bash
 # djfhejhf
 # Puppet manifest to install nginx and configure custom rule
-
-# Install nginx package
-package { 'nginx':
+package {'nginx':
   ensure => 'present',
 }
 
-# Update package list and install nginx
-exec { 'apt-update-install-nginx':
-  command => 'sudo apt-get update && sudo apt-get -y install nginx',
-  path    => '/usr/bin',
-  require => Package['nginx'], # Ensure nginx package is installed before running this command
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
 }
 
-# Create index.html with "Hello World!"
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
+exec {'Hello World!':
+  command  => 'echo "Hello World!" | sudo dd status=none of=/var/www/html/index.html',
+  provider => shell,
 }
 
-# Define Nginx custom configuration using a separate file
-file { '/etc/nginx/sites-available/custom.conf':
-  ensure  => file,
-  content => "\
-server {
-    listen 80 default_server;
-    location /redirect_me {
-        return 301 https://www.youtube.com/;
-    }
-}",
-  notify  => Exec['reload-nginx'], # Notify the 'reload-nginx' exec when this file changes
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
 }
 
-# Link custom.conf to sites-enabled
-file { '/etc/nginx/sites-enabled/custom.conf':
-  ensure => link,
-  target => '/etc/nginx/sites-available/custom.conf',
-  require => File['/etc/nginx/sites-available/custom.conf'], # Ensure custom.conf is created before linking
-  notify => Exec['reload-nginx'], # Notify the 'reload-nginx' exec when this link changes
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
-
-# Reload Nginx configuration
-exec { 'reload-nginx':
-  command  => 'sudo systemctl reload nginx',
-  path     => '/usr/bin',
-  provider => 'shell',
-  refreshonly => true, # Only run this exec when notified by a file change
-}
-
