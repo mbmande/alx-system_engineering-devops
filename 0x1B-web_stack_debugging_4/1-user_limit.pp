97cda8112c8526b2906c755073efe6128789dbe1
mbmande <mbsmande@gmail.com>
2024-08-23 01:40:30 +0000

    stack1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+# ========================
+# ===============================
+
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
